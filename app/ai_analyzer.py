f0ed7e3441700937a788b2449916d83a4cc2d1f4
AAA1603040404 <w4cj5w5b75@privaterelay.appleid.com>
2026-05-24 00:56:59 +0300

    Update ai_analyzer.py

--- app/ai_analyzer.py
+++ app/ai_analyzer.py
@@ -4,7 +4,7 @@ import re
 from google import genai
 from google.genai import types
 
-from app.config import settings
+from config import settings
 
 
 client = genai.Client(api_key=settings.GEMINI_API_KEY)
