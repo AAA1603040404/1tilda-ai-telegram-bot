b9baa536ab0e90efae0e746128f7f85872943f68
w4cj5w5b75-lang <w4cj5w5b75@privaterelay.appleid.com>
2026-05-23 23:43:15 +0300

    Create main.py

---
+++ app/main.py
@@ -0,0 +1,24 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+
+from app.config import settings
+from app.bot_handlers import router
+
+
+async def main():
+    logging.basicConfig(level=logging.INFO)
+
+    bot = Bot(token=settings.BOT_TOKEN)
+    dp = Dispatcher()
+
+    dp.include_router(router)
+
+    logging.info("Bot started")
+
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
